db6e65b47db29d9957c28ff44f7124a9a8ac3984
varundahake <77110699+varundahake@users.noreply.github.com>
2021-07-30 20:00:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas, backgroundimage;
+var gameState = 0;
+var playerCount;
+var database;
+var form, player, game;
+
+function setup(){
+  createCanvas(400, 400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  
+}
